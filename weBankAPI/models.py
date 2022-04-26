e1b583a3dd5adf7c72d8c9d1eafe76ec9a7d6cf5
Akorede olawole <akoredeolawole@gmail.com>
2022-04-26 23:33:14 +0100

    modified models

--- weBankAPI/models.py
+++ weBankAPI/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.contrib.auth.models import User
 
 ACCOUNT_TYPE = (
     ('SAVINGS', 'savings'),
@@ -9,7 +10,7 @@ ACCOUNT_TYPE = (
 
 # Create your models here.
 class Accounts(models.Model):
-    # customer_id = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     account_no = models.IntegerField(validators=[MinLengthValidator(10),MaxLengthValidator(10)])
     account_type = models.CharField(max_length=40, choices=ACCOUNT_TYPE)
     account_balance = models.FloatField(default=0)
